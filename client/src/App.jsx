cef7bfc1bca053cf7a56ef34d14796d5052e474d
Narmathaa JB <balanarmathaa@gmail.com>
2024-04-08 11:53:09 +0530

    page folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import './App.css';
+import Layout from './Layout.jsx';
+import IndexPage from './pages/IndexPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+
 
 function App() {
   return (
-    <div></div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index element={<IndexPage/>} />
+      <Route path="/login" element={<LoginPage/>} />
+      </Route>
+    </Routes>
+    
   )
 }
 
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+export default function LoginPage(){
+    return (
+        <div className="mt-4">
+            <h1 className="text-4xl">Login</h1>
+            <form>
+                <input type="email" placeholder="your@email.com" />
+                <input type="password" placeholder="password" />
+                <button>Login</button>
+            </form>
+        </div>
+    );
+}
