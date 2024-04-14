161406bbeb37fcbd271830615b597186d5209d99
Narmathaa JB <balanarmathaa@gmail.com>
2024-04-14 21:36:16 +0530

    Account page

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,37 +1,14 @@
 import { useContext } from "react";
-import { Link, Navigate, useParams } from "react-router-dom";
 import { UserContext } from "../UserContext.jsx";
+import {Link, Navigate, useParams} from "react-router-dom";
 
 export default function AccountPage(){
-    const {ready,user} = useContext(UserContext);
-    let {subpage} = useParams();
-    if (subpage === undefined) {
-        subpage = 'profile';
-    }
-
-    
-    
-    
-    
-    if (ready && !user) {
-        return <Navigate to={'/login'} />
-    }
-
-    function linkClasses (type=null){
-        let classes = 'py-2 px-6';
-        if (type === subpage) {
-            classes += 'bg-primary text-white rounded-full';
-        }
-        return classes;
-    }
-
-
     return(
         <div>
             <nav className="w-full flex justify-center mt-8 gap-2">
-                <Link className= {linkClasses('profile')} to={'/account'}>My Profile</Link>
-                <Link className= {linkClasses('bookings')} to={'/account/bookings'}>My bookings</Link>
-                <Link className= {linkClasses('places')} to={'/account/places'}>My accommodations</Link>
+                <Link className="py-2 px-6 bg-primary text-white rounded-full" to={'/account'}>My Profile</Link>
+                <Link className="py-2 px-6 bg-primary text-white rounded-full" to={'/account/bookings'}>My bookings</Link>
+                <Link className="py-2 px-6 bg-primary text-white rounded-full" to={'/account/places'}>My accommodations</Link>
             </nav>
         </div>
     );
